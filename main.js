e4f102fb5d561496564ae51e230b46fa31c8603d
antoniozacca <antoniozaccaria057@gmail.com>
2024-05-14 16:39:46 +0200

    finish bonus 1

--- main.js
+++ main.js
@@ -1,45 +1,56 @@
 'use strict'
-let wayneBarnett = {
-    role : "Founder & CEO",
-    image: "img/wayne-barnett-founder-ceo.jpg"
-};
-console.log(wayneBarnett);
-let ul = document.getElementById("primo");
-for (let i=0; i < wayneBarnett.lenght; i++){
+let users = [
+    {
+        name: "Wayne Barnett",
+        role : "Founder & CEO",
+        image: "img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role : "Chief Editor",
+        image: "img/angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role : "Office Manager",
+        image: "img/walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role : "Social Media Manager",
+        image: "img/angela-lopez-social-media-manager.jpg"
+    
+    },
+    {
+        name: "Scott Estrada",
+        role : "Developer",
+        image: "img/scott-estrada-developer.jpg"
+    
+    },
+    {
+        name: "Barbara Ramos",
+        role : "Graphic Designer",
+        image: "img/barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+for(let i=0 ; i < users.length; i++) {
+    let ul = document.getElementById("membri");
     let li = document.createElement("li");
-    li = wayneBarnett[i];
+    li.innerText = users[i].name;
     ul.appendChild(li);
 }
+ for(let i=0 ; i < users.length; i++) {
+     let ul = document.getElementById("membri");
+     let li = document.createElement("li");
+     li.innerText = users[i].role;
+     ul.appendChild(li);
+ }
+ for(let i=0 ; i < users.length; i++) {
+     let ul = document.getElementById("membri");
+     let li = document.createElement("img");
+     li.src = users[i].image;
+     ul.appendChild(li);
+ }
 
-
-const angelaCaroll = {
-    role : "Chief Editor",
-    image: "img/angela-caroll-chief-editor.jpg"
-}
-console.log(angelaCaroll);
-
-const walterGordon = {
-    role : "Office Manager",
-    image: "img/walter-gordon-office-manager.jpg"
-}
-console.log(walterGordon);
-
-const angelaLopez = {
-    role : "Social Media Manager",
-    image: "img/angela-lopez-social-media-manager.jpg"
-
-}
-console.log(angelaLopez);
-
-const scottEstrada = {
-    role : "Developer",
-    image: "img/scott-estrada-developer.jpg"
-
-}
-console.log(scottEstrada);
-
-const barbaraRamos = {
-    role : "Graphic Designer",
-    image: "img/barbara-ramos-graphic-designer.jpg"
-}
-console.log(barbaraRamos);
+    

22198e68b91acc2a58697121c3451ea6fbc2c840
antoniozacca <antoniozaccaria057@gmail.com>
2024-05-14 18:41:26 +0200

    finish exercise

--- main.js
+++ main.js
@@ -35,22 +35,44 @@ let users = [
 ];
 
 for(let i=0 ; i < users.length; i++) {
+    const user = users[i];
     let ul = document.getElementById("membri");
     let li = document.createElement("li");
-    li.innerText = users[i].name;
+    li.classList.add("col-4")
+    li.classList.add("text-center")
+    
+    // image
+    let img = document.createElement("img");
+    img.classList.add("p-5");
+    img.src = user.image;
+    li.appendChild(img);
+
+    // name
+    let h3 = document.createElement("h3");
+    h3.append(user.name)
+    li.appendChild(h3);
+
+    // role
+    let p = document.createElement("p");
+    p.append(user.role);
+    li.append(p);
+
     ul.appendChild(li);
+    
 }
- for(let i=0 ; i < users.length; i++) {
-     let ul = document.getElementById("membri");
-     let li = document.createElement("li");
-     li.innerText = users[i].role;
-     ul.appendChild(li);
- }
- for(let i=0 ; i < users.length; i++) {
-     let ul = document.getElementById("membri");
-     let li = document.createElement("img");
-     li.src = users[i].image;
-     ul.appendChild(li);
- }
+
+// for(let i=0 ; i < users.length; i++) {
+//     let ul = document.getElementById("membri");
+//     let li = document.createElement("li");
+//     li.innerText = users[i].name;
+//     ul.appendChild(li);
+// }
+//  for(let i=0 ; i < users.length; i++) {
+//      let ul = document.getElementById("membri");
+//      let li = document.createElement("li");
+//      li.innerText = users[i].role;
+//      ul.appendChild(li);
+//  }
+ 
 
     
